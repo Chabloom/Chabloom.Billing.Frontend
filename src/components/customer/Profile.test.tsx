5f92f55244a314b1748c26f89aae524a1da7c458
Matthew Casey <mdcasey@chabloom.com>
2020-07-14 10:00:56 -0400

    Remove unneeded component

--- src/components/customer/Profile.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Profile from "./Profile";
-
-test('Render Profile component', () => {
-    renderer.create(<Profile/>);
-});
