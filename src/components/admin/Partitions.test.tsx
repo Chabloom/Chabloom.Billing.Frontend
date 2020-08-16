03471e2c381ee73061be23462f6d5b126a719b97
Matthew Casey <mdcasey@chabloom.com>
2020-08-16 00:53:05 -0400

    Remove partition components

--- src/components/admin/Partitions.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Partitions from "./Partitions";
-
-test('Render Partitions component', () => {
-    renderer.create(<Partitions/>);
-});
