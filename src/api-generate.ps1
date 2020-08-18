0e270f2de2aa0d65bf7764b4540f631bf6ee535a
Matthew Casey <mdcasey@chabloom.com>
2020-08-17 23:49:08 -0400

    Rename API document

--- src/api-generate.ps1
+++ src/api-generate.ps1
@@ -1,2 +1,2 @@
 npm install -g @openapitools/openapi-generator-cli@cli-5.0.0-beta
-openapi-generator generate -i api.json -g typescript-fetch -o api -c api.config.json
+openapi-generator generate -i chabloom-payments-api.json -g typescript-fetch -o api -c api.config.json
--- src/api-generate.sh
+++ src/api-generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 npm install -g @openapitools/openapi-generator-cli@cli-5.0.0-beta
-openapi-generator generate -i api.json -g typescript-fetch -o api -c api.config.json
+openapi-generator generate -i chabloom-payments-api.json -g typescript-fetch -o api -c api.config.json
