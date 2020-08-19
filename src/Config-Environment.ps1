656a915fbac943a8812809ba1e0dc3b544356212
Matthew Casey <mdcasey@chabloom.com>
2020-08-18 22:42:55 -0400

    Change config to typescript file

--- src/Config-Environment.ps1
+++ src/Config-Environment.ps1
@@ -1,6 +1,6 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location $dir
-((Get-Content -Path config.json -Raw) -replace 'API_PUBLIC_ADDRESS',$env:API_PUBLIC_ADDRESS) | Set-Content -Path config.json
-((Get-Content -Path config.json -Raw) -replace 'JWT_PUBLIC_ADDRESS',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path config.json
+((Get-Content -Path settings/config.ts -Raw) -replace 'API_PUBLIC_ADDRESS',$env:API_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
+((Get-Content -Path settings/config.ts -Raw) -replace 'JWT_PUBLIC_ADDRESS',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 Pop-Location
---
+++ src/settings/config.ts
@@ -0,0 +1,6 @@
+export const ApplicationConfig = {
+    "name": "chabloom-payments",
+    "displayName": "Chabloom Payments",
+    "apiPublicAddress": "API_PUBLIC_ADDRESS",
+    "jwtPublicAddress": "JWT_PUBLIC_ADDRESS"
+}
