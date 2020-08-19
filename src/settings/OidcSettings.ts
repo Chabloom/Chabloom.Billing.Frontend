6108dbd50fabda067cd974f10d84cd970771197c
Matthew Casey <mdcasey@chabloom.com>
2020-08-19 00:01:13 -0400

    Update oidc settings

--- src/settings/OidcSettings.ts
+++ src/settings/OidcSettings.ts
@@ -5,8 +5,8 @@ import {ApplicationConfig} from "./config";
 export const OidcSettings: UserManagerSettings = {
     authority: ApplicationConfig.jwtPublicAddress,
     client_id: "Chabloom.Payments.Frontend",
-    redirect_uri: "/signin-oidc",
-    post_logout_redirect_uri: "/signout-oidc",
+    redirect_uri: "https://payments-test.chabloom.com/signin-oidc",
+    post_logout_redirect_uri: "https://payments-test.chabloom.com/signout-oidc",
     response_type: "code",
     scope: "openid profile Chabloom.Payments",
     filterProtocolClaims: true,
