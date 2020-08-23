e274d8d74d2745a9f7af5c3cf63f72d9c2fa3db1
Matthew Casey <mdcasey@chabloom.com>
2020-08-23 01:57:06 -0400

    Update home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,80 @@
 import React from "react";
 
-import {Grid} from "@material-ui/core";
+import {
+    Button,
+    createStyles,
+    FormGroup,
+    Grid,
+    LinearProgress,
+    Paper,
+    TextField,
+    Theme,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {Alert, AlertTitle} from "@material-ui/lab";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        paper: {
+            padding: theme.spacing(2),
+        },
+        mt1: {
+            marginTop: theme.spacing(1),
+        }
+    }),
+);
 
 export const Home: React.FC = (props) => {
+    const [tenant, setTenant] = React.useState("");
+    const [account, setAccount] = React.useState("");
+    const [error, setError] = React.useState("");
+    const [processing, setProcessing] = React.useState(false);
+
+    const classes = useStyles();
+
     return (
-        <Grid>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Paper elevation={3}>
+                    <Toolbar>
+                        <Typography variant="h6">Chabloom Payments</Typography>
+                    </Toolbar>
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper elevation={3} className={classes.paper}>
+                    <Typography variant="h6">Quick Pay</Typography>
+                    <form>
+                        <FormGroup>
+                            <TextField required name="tenant" label="Tenant" value={tenant}
+                                       disabled={processing} onChange={e => setTenant(e.target.value)}/>
+                            <TextField required name="account" label="Account" value={account}
+                                       disabled={processing} onChange={e => setAccount(e.target.value)}/>
+                        </FormGroup>
+                        {error &&
+                        <Alert className={classes.mt1} severity="error">
+                            <AlertTitle>Error</AlertTitle>
+                            {error}
+                        </Alert>
+                        }
+                        {processing &&
+                        <LinearProgress className={classes.mt1}/>
+                        }
+                        <Button className={classes.mt1} variant="contained" color="primary" type="submit"
+                                disabled={processing}>
+                            Get Bills
+                        </Button>
+                    </form>
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper elevation={3} className={classes.paper}>
+                    <Typography variant="h6">Upcoming Bills</Typography>
+                </Paper>
+            </Grid>
         </Grid>
+
     );
 }
