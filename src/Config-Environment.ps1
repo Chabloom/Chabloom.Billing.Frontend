afb336daa28d4fbd7bc794bc47208a6583ad50ac
Matthew Casey <mdcasey@chabloom.com>
2020-10-01 11:01:55 -0400

    Use new app config in OIDC settings

--- src/Config-Environment.ps1
+++ src/Config-Environment.ps1
@@ -1,6 +1,7 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location $dir
+((Get-Content -Path settings/config.ts -Raw) -replace 'PUBLIC_ADDRESS',$env:PUBLIC_ADDRESS) | Set-Content -Path settings/oidc.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'API_PUBLIC_ADDRESS',$env:API_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'JWT_PUBLIC_ADDRESS',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 Pop-Location
--- src/settings/oidc.ts
+++ src/settings/oidc.ts
@@ -1,12 +1,12 @@
 import {UserManagerSettings} from "oidc-client";
 
-import {ApplicationConfig} from "./config";
+import {AppConfig} from "./config";
 
 export const OidcSettings: UserManagerSettings = {
-    authority: ApplicationConfig.jwtPublicAddress,
+    authority: AppConfig.jwtPublicAddress,
     client_id: "Chabloom.Payments.Frontend",
-    redirect_uri: "https://payments-test.chabloom.com/signin-oidc",
-    post_logout_redirect_uri: "https://payments-test.chabloom.com/signout-oidc",
+    redirect_uri: "PUBLIC_ADDRESS/signin-oidc",
+    post_logout_redirect_uri: "PUBLIC_ADDRESS/signout-oidc",
     response_type: "code",
     scope: "openid profile Chabloom.Payments",
     filterProtocolClaims: true,
