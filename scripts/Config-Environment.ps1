3bcd3f9d1cff24819242e8ad5a09dd8fc5729987
Matthew Casey <mdcasey@chabloom.com>
2020-11-10 10:05:20 -0500

    Move environment config script to scripts directory

--- scripts/Config-Environment.ps1
+++ scripts/Config-Environment.ps1
@@ -1,6 +1,6 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
-Push-Location $dir
+Push-Location $dir/../src/
 ((Get-Content -Path settings/oidc.ts -Raw) -replace 'http://localhost:3001',$env:PUBLIC_ADDRESS) | Set-Content -Path settings/oidc.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'https://payments-api-test.chabloom.com',$env:API_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
