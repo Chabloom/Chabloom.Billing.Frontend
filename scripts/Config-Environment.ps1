085a0439b626056024f40b5a45b6950c7b79205e
Matthew Casey <mdcasey@chabloom.com>
2020-11-16 00:52:52 -0500

    Update environment config script

--- scripts/Config-Environment.ps1
+++ scripts/Config-Environment.ps1
@@ -1,9 +1,9 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location $dir/../src/
-((Get-Content -Path types/settings.ts -Raw) -replace 'http://localhost:3001',$env:PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://payments-api-test.chabloom.com',$env:API_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'http://localhost:3002',$env:PROCESSING_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:ACCOUNTS_API_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'https://payments-api-test.chabloom.com',$env:PAYMENTS_API_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'https://processing-api-test.chabloom.com',$env:PROCESSING_API_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
 ((Get-Content -Path types/settings.ts -Raw) -replace 'APPINSIGHTS_INSTRUMENTATIONKEY',$env:APPINSIGHTS_INSTRUMENTATIONKEY) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'http://localhost:3000',$env:PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
 Pop-Location
