5bed1cb4c6eb10861027803be237d38070bbb3d4
Matthew Casey <mdcasey@chabloom.com>
2021-02-03 22:04:50 -0500

    Fix unresolved reference

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -28,7 +28,7 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(ApplicationConfig.publicAddress, window.location.href);
+    const publicUrl = new URL(ApplicationConfig.frontendPublicAddress, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -37,7 +37,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${ApplicationConfig.publicAddress}/service-worker.js`;
+      const swUrl = `${ApplicationConfig.frontendPublicAddress}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
