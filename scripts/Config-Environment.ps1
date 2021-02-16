40fd33252582065a2104413a3973a0acbab52b28
Matthew Casey <mdcasey@chabloom.com>
2021-02-16 00:05:38 -0500

    Update config script

--- scripts/Config-Environment.ps1
+++
@@ -1,8 +0,0 @@
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-Push-Location $dir/../src/
-((Get-Content -Path types/settings.ts -Raw) -replace 'http://localhost:3001',$env:BILLING_FRONTEND_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://billing-api-test.chabloom.com',$env:BILLING_BACKEND_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:ACCOUNTS_BACKEND_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'APPINSIGHTS_INSTRUMENTATIONKEY',$env:APPINSIGHTS_INSTRUMENTATIONKEY) | Set-Content -Path types/settings.ts
-Pop-Location
