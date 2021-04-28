8c13feaa90918e48784200e84b5e43623ceea855
Matthew Casey <mdcasey@chabloom.com>
2021-04-28 16:32:59 -0400

    Update docker build

--- docker-build-dev-1.sh
+++ docker-build-dev-1.sh
@@ -1,2 +1,4 @@
-docker build -t 10.1.1.11:32000/chabloom-billing-frontend:1 .
-docker push 10.1.1.11:32000/chabloom-billing-frontend:1
+timestamp=`date +%s`
+docker build -t 10.1.1.11:32000/chabloom-billing-frontend:$timestamp -t 10.1.1.11:32000/chabloom-billing-frontend:latest .
+docker push 10.1.1.11:32000/chabloom-billing-frontend:$timestamp
+docker push 10.1.1.11:32000/chabloom-billing-frontend:latest
