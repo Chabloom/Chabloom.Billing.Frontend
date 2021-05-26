2b84e277c687a5bc13435a5fe61a89cc7908a813
Matthew Casey <mdcasey@chabloom.com>
2021-05-26 01:12:41 -0400

    Update manager exports

--- src/components/Manager/index.ts
+++ src/components/Manager/index.ts
@@ -1,8 +1,6 @@
 export * from "./Dashboard";
 
 export * from "./Account";
-export * from "./ApplicationUser";
 export * from "./Bill";
 export * from "./BillSchedule";
 export * from "./Tenant";
-export * from "./TenantUser";
