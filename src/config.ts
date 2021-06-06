ae655dd98cd495adde35b4032b6d56c1d3d16cc5
Matthew Casey <mdcasey@chabloom.com>
2021-06-05 21:55:05 -0400

    Update OIDC configuration

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 import { UserManagerSettings } from "oidc-client";
 
 export const OidcConfiguration: UserManagerSettings = {
-  authority: window.__env__.REACT_APP_ACCOUNTS_BACKEND_ADDRESS,
+  authority: window.__env__.REACT_APP_BILLING_BACKEND_ADDRESS,
   client_id: "Chabloom.Billing.Frontend",
   redirect_uri: `${window.__env__.REACT_APP_BILLING_FRONTEND_ADDRESS}/signin-oidc`,
   post_logout_redirect_uri: `${window.__env__.REACT_APP_BILLING_FRONTEND_ADDRESS}/signout-oidc`,
   response_type: "code",
-  scope: "openid profile Chabloom.Accounts.Backend Chabloom.Billing.Backend Chabloom.Transactions.Backend",
+  scope: "openid profile Chabloom.Billing.Backend",
   filterProtocolClaims: true,
   loadUserInfo: true,
 };
